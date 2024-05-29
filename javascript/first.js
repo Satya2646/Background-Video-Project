d842f0ae5cf9d8fd9038adad471d8ab0089c245e
Satya2646 <23mca054@abit.edu.in>
2024-05-29 12:39:01 +0530

    Initial commit

---
+++ javascript/first.js
@@ -0,0 +1,12 @@
+//in javascript variables are declared by three types
+//they are let , const, var
+let name = "tony stark";
+console.log(name);
+let age = 24;
+console.log(age);
+var fullName = "tony stark";
+console.log(fullName);
+const a=54;
+console.log(a);
+
+//var declared can be changed but const value cannot be changed ;
